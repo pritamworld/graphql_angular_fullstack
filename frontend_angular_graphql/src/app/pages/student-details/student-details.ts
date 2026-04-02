57c46c35cab77461b0e71218d3ece091e5bfdf96
moxDroid Labs Inc <pritam.world@gmail.com>
2026-04-01 20:35:18 -0400

    code changes

--- frontend_angular_graphql/src/app/pages/student-details/student-details.ts
+++ frontend_angular_graphql/src/app/pages/student-details/student-details.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { StudentService } from '../../services/student-service';
 
 @Component({
   selector: 'app-student-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './student-details.html',
   styleUrl: './student-details.css',
 })
-export class StudentDetails {
+export class StudentDetails implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly studentService = inject(StudentService);
 
+  student: any = null;
+  loading = false;
+  error: string | null = null;
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('studentId');
+
+    console.log('Student ID from route:', id);
+    
+    if (id) {
+      this.loadStudent(id);
+    } else {
+      this.error = 'Invalid student ID';
+    }
+  }
+
+  loadStudent(id: string) {
+    this.loading = true;
+
+    this.studentService.getStudentById(id).subscribe({
+      next: (data) => {
+        console.log('Student Details:', data);
+        this.student = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = err.message;
+        this.loading = false;
+      },
+    });
+  }
 }
